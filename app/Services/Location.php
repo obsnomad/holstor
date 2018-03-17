9e969860835bcce11b15efa131b7ab33fea1f50e
akatelnikov <akatelnikov@corp.acesse.com>
2018-03-17 22:18:31 +0300

    Locations fix

--- app/Services/Location.php
+++ app/Services/Location.php
@@ -54,7 +54,7 @@ class Location
     public $seo;
     protected $data;
 
-    public function __construct($data)
+    public function __construct($data = null)
     {
         $this->data = (array)$data;
         foreach ([
