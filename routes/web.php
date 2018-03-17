80c3a03e733c31fe0e90b2016af78272ddf957ca
akatelnikov <akatelnikov@corp.acesse.com>
2018-03-17 22:41:16 +0300

    Route redirect

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-Route::group(['domain' => 'холстор.рф'], function() {
-    return redirect(env('APP_URL'));
-});
 Route::get('/', ['as' => 'index', 'uses' => 'PublicController@index']);
 Route::post('/order', ['as' => 'order', 'uses' => 'PublicController@order']);
 Route::post('/franch', ['as' => 'franch', 'uses' => 'PublicController@franch']);
