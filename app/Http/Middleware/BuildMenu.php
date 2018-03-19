b4a6cd3d41b8925a2e683018396688e3531fa45f
akatelnikov <akatelnikov@corp.acesse.com>
2018-03-19 19:55:08 +0300

    Remove franch

--- app/Http/Middleware/BuildMenu.php
+++ app/Http/Middleware/BuildMenu.php
@@ -21,12 +21,14 @@ class BuildMenu
                 ->add('<span class="fas fa-bars"></span>', ['class' => 'navbar-mobile-menu'])
                 ->link->attr(['id' => 'navbar-mobile-menu']);
         });
-        Menu::make('menuMain', function ($menu) {
+        Menu::make('menuMain', function ($menu) use ($request) {
             $menu->add('Главная', ['url' => '#main']);
             $menu->add('Цены', ['url' => '#prices']);
             $menu->add('Отзывы', ['url' => '#reviews']);
             $menu->add('Контакты', ['url' => '#contacts']);
-            $menu->add('Франшиза', ['url' => '#franch']);
+            if (!$request->cookie('location')) {
+                $menu->add('Франшиза', ['url' => '#franch']);
+            }
         });
         return $next($request);
     }
